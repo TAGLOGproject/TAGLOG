69f8682c40c380cc7201096993159344ea4ed75f
ssori <115159126+ssori0421@users.noreply.github.com>
2023-06-09 19:21:50 +0900

    Header css 작업 중- 데스크톱 버전 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image';
 import styles from './page.module.scss';
 
 export default function Home(props: any) {
   return (
     <main className={styles.main}>
-      <div>dd</div>
+      <header className={styles.headerContainer}>
+        <div className={styles.headerWrapper}>
+          <h2 className={styles.title}>TAGLOG</h2>
+          <button className={styles.toggleButton} type="button">
+            Button
+          </button>
+        </div>
+      </header>
     </main>
   );
 }
