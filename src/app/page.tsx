dfda61119bca75199826696e2f0e928af0149765
ssori <115159126+ssori0421@users.noreply.github.com>
2023-06-11 00:28:48 +0900

    feat: Header 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import styles from './page.module.scss';
-import Hamburger from '../assets/svg/hamburger.svg';
+import Header from '@/components/header/Header';
 
 export default function Home(props: any) {
-  return (
-    <main className={styles.main}>
-      <header className={styles.headerContainer}>
-        <div className={styles.headerWrapper}>
-          <h2 className={styles.title}>TAGLOG</h2>
-          <Hamburger />
-        </div>
-      </header>
-    </main>
-  );
+  return <Header />;
 }
