b0293b250e96cfa1c1f58f7dec4483668e00ba1d
ssori <115159126+ssori0421@users.noreply.github.com>
2023-06-11 00:29:07 +0900

    DropDown 작업 중

---
+++ src/components/header/DropDown.tsx
@@ -0,0 +1,14 @@
+function DropDown() {
+  return (
+    <>
+      <div>버튼</div>
+      <ul>
+        <li>settings</li>
+        <li>내가 작성한 글</li>
+        <li>로그아웃</li>
+      </ul>
+    </>
+  );
+}
+
+export default DropDown;
