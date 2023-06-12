60aa20071cb702add5a739d60f37e80ddad105cd
joonhyuk <anshqhsh.dev@gmail.com>
2023-06-12 12:31:18 +0900

    chore: page를 clientSide로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+'use client';
+
+import useResponsive from '@/hooks/useResponsive';
+
 export default function Home(props: any) {
-  return <div>home</div>;
+  const { isDesktop, windowWidth } = useResponsive();
+
+  return (
+    <div>
+      home
+      {isDesktop && <div>desktop</div>}
+      {!isDesktop && <div>mobile</div>}
+      {windowWidth}
+    </div>
+  );
 }
