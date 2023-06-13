05b054bfbd54fd9c17f031ecbe717ce60ce7da90
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-06-13 12:14:31 +0900

    Feat/useresponse (#13)
    * feat: useResponse hook 추가(작업 중)
    
    * chore: css stylelint formatting
    
    * chore: add constants values
    
    * feat: useResponsive hook enhancement
    
    * comment: useResponsive hook 관련 comment 추가
    
    * chore: page를 clientSide로 수정
    
    * chore: width 상수 값 사용

---
+++ src/hooks/useResponsive.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { TABLET_WIDTH } from '@/constants';
+import { useEffect, useState } from 'react';
+
+/**
+ * useResponsive
+ * @description 반응형 웹을 위한 커스텀 훅
+ * isDesktop: 데스크탑 여부
+ * windowWidth: 창 너비
+ * @returns {object} { isDesktop: boolean, windowWidth: number }
+ */
+const useResponsive = () => {
+  const [isDesktop, setIsDesktop] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const { innerWidth } = window;
+      const isDesktopWidth = innerWidth > TABLET_WIDTH;
+      setIsDesktop(isDesktopWidth);
+      setWindowWidth(innerWidth);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize); // 창 크기 변경 이벤트 감지
+
+    return () => {
+      window.removeEventListener('resize', handleResize); // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    };
+  }, []);
+
+  return { isDesktop, windowWidth };
+};
+
+export default useResponsive;
