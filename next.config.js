8c244f5cd7cc94c8c60b7936aed8ba839cc54543
joonhyuk <anshqhsh.dev@gmail.com>
2023-06-22 21:56:59 +0900

    feat: mongoDB settings

--- next.config.js
+++ next.config.js
@@ -3,12 +3,20 @@ const nextConfig = {
   compress: true,
   experimental: {
     appDir: true,
+    esmExternals: 'loose',
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  future: {
+    webpack5: false, // 또는 false
   },
   webpack: (config) => {
+    // eslint-disable-next-line no-param-reassign
+    config.experiments = { ...config.experiments, topLevelAwait: true };
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
     return config;
   },
 };
