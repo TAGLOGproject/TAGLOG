2946960b07015163bd44b3e7da2c8b3075983531
anshqhsh <anshqhsh.dev@gmail.com>
2023-07-25 14:35:09 +0900

    feat: react quill 사용할려 했으나, quill 자체에서 Md 형식을 지원x

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,46 +1,90 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-import { useState } from 'react';
+import QuillNoSSRWrapper from '@/components/QuillNoSSRWrapper';
+import { useEffect, useRef, useState } from 'react';
+import ReactQuill from 'react-quill';
+import ReactMarkdown from 'react-markdown';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const QuillNoSSRWrapper = dynamic(() => import('react-quill'), {
-  ssr: false,
-  loading: () => <p>Loading ...</p>,
-});
+import MdEditor from 'react-markdown-editor-lite';
+import 'react-markdown-editor-lite/lib/index.css';
+import { HtmlType } from 'react-markdown-editor-lite/cjs/editor/preview';
+import MarkdownIt from 'markdown-it';
 
-const modules = {
-  toolbar: [
-    [{ header: '1' }, { header: '2' }, { header: '3' }, { header: '4' }, { font: [] }],
-    [{ size: [] }],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-    ['link', 'image', 'video'],
-    ['clean'],
-  ],
-  clipboard: {
-    // toggle to add extra line breaks when pasting HTML:
-    matchVisual: false,
-  },
-};
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
 
-function TextEditor() {
-  const [content, setContent] = useState('');
-  const [isDraft, setIsDraft] = useState(true);
-  const [isPublished, setIsPublished] = useState(false);
+export default function Editor() {
+  const [contents, setContents] = useState<string>();
+  const [mdEditorContents, setMdEditorContents] = useState<string>();
+  const quillRef = useRef<ReactQuill>(null);
 
-  function submitHandler(event: any) {
-    event.preventDefault();
-  }
+  const modules = {
+    toolbar: [
+      ['link', 'image', 'video'],
+      [{ header: [1, 2, 3, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+
+  const handleEditorChange = ({ text }) => {
+    setMdEditorContents(text);
+  };
+  console.log(quillRef);
+  useEffect(() => {
+    const handleImage = () => {
+      const input = document.createElement('input');
+      console.log({ input });
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+      input.click();
+      input.onchange = async () => {
+        const file = input.files[0];
+        console.log(file);
+
+        // 현재 커서 위치 저장
+        const range = quillRef.current!.getEditor().getSelection(true);
+
+        // 서버에 올려질때까지 표시할 로딩 placeholder 삽입
+        quillRef.current!.getEditor().insertEmbed(range.index, 'image', `/images/loading.gif`);
+      };
+    };
+
+    if (quillRef.current) {
+      const toolbar = quillRef.current.getEditor().getModule('toolbar');
+      toolbar.addHandler('image', handleImage);
+    }
+  }, []);
+
+  const onChangeQuill = (content: string) => {
+    setContents(content);
+  };
 
   return (
-    <form onSubmit={submitHandler}>
-      {/* <label>Title</label> */}
-      <QuillNoSSRWrapper modules={modules} onChange={setContent} theme="snow" />
-      <button type="button">Save</button>
-      <p>{content}</p>
-    </form>
+    <div>
+      {/* <QuillNoSSRWrapper modules={modules} forwardedRef={quillRef} value={contents} />; */}
+      <ReactQuill modules={modules} ref={quillRef} value={contents} onChange={onChangeQuill} />
+      {contents}
+      {/* <ReactQuill modules={modules} value={contents} readOnly />
+      마크다운
+      <ReactMarkdown>{contents}</ReactMarkdown> */}
+      <MdEditor
+        plugins={['image']}
+        value={mdEditorContents}
+        style={{ height: '500px' }}
+        onChange={handleEditorChange}
+        renderHTML={(text) => mdParser.render(text)}
+        onImageUpload={(file: any) => {
+          console.log(file);
+        }}
+      />
+    </div>
   );
 }
-
-export default TextEditor;
