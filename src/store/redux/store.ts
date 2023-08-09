208e2a812a693f2941dbc29e67c5501efbe87e9b
ssori <115159126+ssori0421@users.noreply.github.com>
2023-08-09 21:11:29 +0900

    feat: store 수정

--- src/store/redux/store.ts
+++ src/store/redux/store.ts
@@ -1,15 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/dist/query';
+// root reducer를 import
 import themeReducer from './slice/themeSlice';
 
+// configureStore()함수를 사용해서 store 생성
+// root reducer를 지정하고 (리듀서들을 결합)
+// (미들웨어 설정을 커스터마이징)
+// (데브툴)
 export const store = configureStore({
   reducer: {
     themeReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
-
-setupListeners(store.dispatch);
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
