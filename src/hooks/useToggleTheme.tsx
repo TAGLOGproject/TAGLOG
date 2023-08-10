c23179a7dd0596f238f36286d2c00dddac9549a8
ssori <115159126+ssori0421@users.noreply.github.com>
2023-08-10 10:47:41 +0900

    feat: dispatch() 함수 작성

--- src/hooks/useToggleTheme.tsx
+++ src/hooks/useToggleTheme.tsx
@@ -1,14 +1,22 @@
-'use client';
-
-import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/store/redux';
+import { toggleTheme } from '@/store/redux/slice/themeSlice';
+import { useEffect } from 'react';
 
 const useToggleTheme = () => {
-  const [theme, setTheme] = useState<string>('light');
+  // useAppSelector 훅을 사용 >
+  // 구조 분해 할당 사용 >
+  // redux store에서 theme값 추출
+  const { theme } = useAppSelector((state) => state.themeState);
+  // useAppDispatch()함수를 사용해서 dispatch()함수 가져옴
+  const dispatch = useAppDispatch();
 
+  // dispatch()함수를 사용 >
+  // toggleTheme() 액션 생성자 함수를 사용해서 액션 객체를 반환 >
+  // 액션 객체를  redux store에 전달
   const onToggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
+    dispatch(toggleTheme());
   };
+
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
   }, [theme]);
