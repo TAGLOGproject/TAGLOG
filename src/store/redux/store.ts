839e91bd0c87ed8c26112ec415e7d81f6a4d2dbb
ssori <115159126+ssori0421@users.noreply.github.com>
2023-08-10 10:45:47 +0900

    feat: redux store 생성 및 redux-persist를 사용

--- src/store/redux/store.ts
+++ src/store/redux/store.ts
@@ -1,14 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
-// root reducer를 import
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import themeReducer from './slice/themeSlice';
 
-// configureStore()함수를 사용해서 store 생성
-// root reducer를 지정하고 (리듀서들을 결합)
-// (미들웨어 설정을 커스터마이징)
-// (데브툴)
-export const store = configureStore({
-  reducer: {
-    themeReducer,
-  },
-  devTools: process.env.NODE_ENV !== 'production',
+// 지속(psersist)할 설정값(configuration) 설정하기
+const persistConfig = {
+  key: 'root', // 설정 키
+  storage, // 저장할 스토리지 (기본값: 로컬스토리지)
+  whitelist: ['themeState'], // 저장할 reducer
+};
+
+// combineReducers({})함수를 사용 >
+// redux store에 있는 다양한 reducer들을 합쳐서 하나의 객체로 반환
+const rootReducer = combineReducers({
+  themeState: themeReducer,
 });
+
+// persistReducer(config, reducer)함수를 사용 >
+// config 객체와 rootReducer를 인자로 받아서 >
+// config 객체에 설정을 확인 >
+// 작성된 stoaga에 whitelist에 작성된 reducer만 선택적으로 storage에 저장
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// configureStore({reducer,middleware,devTools})함수를 사용 >
+// redux store생성 및 다양한 설정
+export const store = configureStore({
+  reducer: persistedReducer, // 루트 리듀서
+  middleware: (
+    getDefaultMiddleware // 미들웨어 설정
+  ) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: process.env.NODE_ENV !== 'production', // 개발환경에서 Redux DevTools(개발자 도구)와 연동
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
