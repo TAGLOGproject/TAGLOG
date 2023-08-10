df62f29694ec859172107afbe828d7cb8fcb78b9
ssori <115159126+ssori0421@users.noreply.github.com>
2023-08-10 11:08:36 +0900

    feat: useToggleTheme 훅 사용 제거

--- src/components/ToggleThemeButton/index.tsx
+++ src/components/ToggleThemeButton/index.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import Image from 'next/image';
-import useToggleTheme from '@/hooks/useToggleTheme';
+import { useAppDispatch, useAppSelector } from '@/store/redux';
+import { toggleTheme } from '@/store/redux/slice/themeSlice';
 import styles from './toggleThemeButton.module.scss';
 
 export default function ToggleThemeButton() {
-  const { theme, onToggleTheme } = useToggleTheme();
+  const { theme } = useAppSelector((state) => state.themeState);
+
+  const dispatch = useAppDispatch();
+
+  const onToggleTheme = () => {
+    dispatch(toggleTheme());
+  };
+
   return (
     <button type="button" onClick={onToggleTheme} className={styles.toggleThemeBtn}>
       {theme === 'light' ? (
