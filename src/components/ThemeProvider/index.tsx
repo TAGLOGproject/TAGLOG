8d75db2cf2d07629c249d52443c70b7d962f76b4
ssori <115159126+ssori0421@users.noreply.github.com>
2023-08-10 10:49:54 +0900

    feat: useAppSelector훅을 사용해서 store에서 theme값 추출

--- src/components/ThemeProvider/index.tsx
+++ src/components/ThemeProvider/index.tsx
@@ -2,7 +2,10 @@ import { useAppSelector } from '@/store/redux';
 import { ReactNode, useEffect } from 'react';
 
 function ThemeProvider({ children }: { children: ReactNode }): JSX.Element {
-  const theme = useAppSelector((state) => state.themeReducer.theme);
+  // useAppSelector 훅을 사용 >
+  // 구조 분해 할당 사용 >
+  // redux store에서 theme값 추출
+  const { theme } = useAppSelector((state) => state.themeState);
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
