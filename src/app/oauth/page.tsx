33d4a675a49ac05d9b5752f720740b74cd07e0ce
anshqhsh <anshqhsh.dev@gmail.com>
2023-08-15 20:47:19 +0900

    feat: kakao code 연동 front측 코드

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import React from 'react';
+import React, { useEffect } from 'react';
+import axiosInstance from '@/utils/axios';
 
 export default function OAuth() {
   const searchParams = useSearchParams();
 
   const authCode = searchParams?.get('code');
+
+  useEffect(() => {
+    axiosInstance.post('/oauth/kakao-login', { authCode });
+  });
 
   return <div>{authCode}</div>;
 }
