8946925799fc5b8eae8ac2252da7d48de81f0ea1
anshqhsh <anshqhsh.dev@gmail.com>
2023-08-15 20:48:36 +0900

    feat: User Schema 추가

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,25 +1,77 @@
 import mongoose from 'mongoose';
 
+// const scheme = {
+//   type: 'kakao',
+//   userid: '1234567890',
+//   nickname: '김개똥',
+//   email: '',
+//   profile_image: '',
+//   thumbnail_image: '',
+//   birthday: '1022',
+//   birthday_type: 'SOLAR',
+//   isAdmin: true,
+//   created_at: '2021-08-15T04:45:02Z',
+//   connected_at: '2023-08-15T04:45:02Z',
+// };
+
 const UserSchema = new mongoose.Schema({
-  name: {
+  type: {
+    type: String,
+    required: true,
+    trim: true,
+    default: 'kakao',
+    enum: ['kakao', 'default'], // 추후 다른 소셜 로그인을 추가할 수 있습니다.
+  },
+  userid: {
     type: String,
     required: true,
     trim: true,
   },
+  nickname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  // unique 값에대한 처리가 필요할 예정
   email: {
     type: String,
     required: true,
     unique: true,
     trim: true,
   },
-  password: {
+  profile_image: {
     type: String,
+    trim: true,
+  },
+  thumbnail_image: {
+    type: String,
+    trim: true,
+  },
+  birthday: {
+    type: String,
+    trim: true,
+  },
+  birthday_type: {
+    type: String,
+    trim: true,
+    enum: ['SOLAR', 'LUNAR'],
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now, // 현재 날짜/시간을 기본값으로 설정
     required: true,
   },
-  nicname: {
+  connected_at: {
+    type: Date,
+  },
+  password: {
+    // 일반 유저 일때만 필요
     type: String,
-    required: true,
-    trim: true,
+    required: false,
   },
 });
 
