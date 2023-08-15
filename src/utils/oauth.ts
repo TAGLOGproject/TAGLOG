4b0a3cd59295113335a79e463c80d3a9d8db2e40
anshqhsh <anshqhsh.dev@gmail.com>
2023-08-15 20:48:13 +0900

    feat: kakao Token URL 세팅

--- src/utils/oauth.ts
+++ src/utils/oauth.ts
@@ -1,3 +1,8 @@
-const { KAKAO_CLIENT_ID } = process.env;
+export const { KAKAO_CLIENT_ID } = process.env;
 const REDIRECT_URI = 'http://localhost:3000/oauth';
+
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+
+export const TOKEN_URL = (authCode: string) => {
+  return `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${authCode}`;
+};
