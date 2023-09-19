dec054e3802a76c2b992921cf1ecf787c84d5edf
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-09-19 23:11:06 +0900

    chore: constants 파일 분리

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,12 @@
 // responsible layout constants
-const DESKTOP_WIDTH = 1280;
-const TABLET_WIDTH = 768;
-const MOBILE_WIDTH = 360;
+export const DESKTOP_WIDTH = 1280;
+export const TABLET_WIDTH = 768;
+export const MOBILE_WIDTH = 360;
 
-const SIDEBAR_MENUS = ['POST', 'ABOUT', 'ETC'];
+export const SIDEBAR_MENUS = ['POST', 'ABOUT', 'ETC'];
 
 // 라우팅
-const ROUTES = [
+export const ROUTES = [
   { path: '/', name: 'HOME' },
   { path: '/cardView', name: 'CARDVIEW' },
   { path: '/calculator', name: 'POST' },
@@ -15,5 +15,3 @@ const ROUTES = [
   // 임시
   { path: '/calculator', name: 'Calculator' },
 ];
-
-export { DESKTOP_WIDTH, TABLET_WIDTH, MOBILE_WIDTH, ROUTES };
