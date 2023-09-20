fd7119a3b9a4a2bfba250a05f3dfca99b58da1d3
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-21 00:00:52 +0900

    feat: 인가 관련 인스턴스 작성

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { BASE_URL } from '@/constants';
+import { getAccessToken } from '@/utils/localstorage';
 
 const defaultConfig = {
   baseURL: BASE_URL,
@@ -9,3 +10,14 @@ const defaultConfig = {
 };
 
 export const instance = axios.create(defaultConfig);
+
+export const authInstance = axios.create(defaultConfig);
+authInstance.interceptors.request.use((config) => {
+  const accessToken = getAccessToken();
+  const newConfig = { ...config };
+  if (accessToken) {
+    const token = JSON.parse(accessToken);
+    newConfig.headers.Authorization = `Bearer ${token}`;
+  }
+  return newConfig;
+});
