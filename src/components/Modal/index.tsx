9942c57cd8f22fa4fc80112eea29a8b880b65a3f
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-21 22:11:58 +0900

    chore: 모달 관련 사소한 수정

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -13,7 +13,7 @@ export default function Modal() {
   const handleModal = () => {
     setModal();
   };
-
+  console.log({ KAKAO_AUTH_URL });
   return (
     <div className={styles.overlay}>
       <div className={styles.modalContainer}>
@@ -28,16 +28,16 @@ export default function Modal() {
             <Image src="/svgs/cross-mark.png" alt="exitButton" width={25} height={25} />
           </button>
         </div>
-        <button type="button" className={styles.signinBtn}>
-          <a href={KAKAO_AUTH_URL}>
+        <a href={KAKAO_AUTH_URL}>
+          <div className={styles.signinBtn}>
             <Image
               src="/svgs/kakao_login_medium_wide.png"
               alt="signinButton"
               width={200}
               height={40}
             />
-          </a>
-        </button>
+          </div>
+        </a>
       </div>
     </div>
   );
--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -11,12 +11,12 @@ function Provider({ children }: { children: React.ReactNode }) {
   const theme = useStore(useThemeStore, (state) => state.theme);
 
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
+    if (theme) document.documentElement.setAttribute('data-theme', theme);
   });
   return (
     <div>
       {children}
-      {isModalOpen ? <Modal /> : ''}
+      {isModalOpen ? <Modal /> : null}
     </div>
   );
 }
