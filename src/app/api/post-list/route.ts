824359e7bcbca9af9df4ccd5808988f2854134f5
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-10-13 23:02:13 +0900

    chore: Mock data 적용

--- src/app/api/post-list/route.ts
+++ src/app/api/post-list/route.ts
@@ -1,24 +1,30 @@
 import { NextResponse } from 'next/server';
 
 import connectDb from '@/app/lib/dbConnect';
-import { IPOSTLIST } from '@/types/api/post';
+import { IPostListData } from '@/types/api/post';
 
-const MOCK_POST_LIST: IPOSTLIST[] = [
+const MOCK_POST_LIST: IPostListData[] = [
   {
+    id: '23',
+    image: '',
     post_id: '2023101034242245', // timestamp + random
     title: '개발이란',
     subtitle: '개발이란 무엇인가',
     tags: ['개발', '프로그래밍', '코딩'],
-    cretedAt: 'Fri Oct 13 2023 21:51:00 GMT+0900',
+    createdAt: 'Fri Oct 13 2023 21:51:00 GMT+0900',
     editedAt: 'Fri Oct 13 2023 21:51:00 GMT+0900',
+    likeCnt: 0,
   },
   {
+    id: '23',
+    image: '',
     post_id: '2023101034242245', // timestamp + random
-    title: '개발이란2',
-    subtitle: '개발이란 무엇인가2',
-    tags: ['javascript', 'next', 'typescript'],
-    cretedAt: 'Fri Oct 13 2023 21:51:00 GMT+0900',
+    title: '개발이란',
+    subtitle: '개발이란 무엇인가',
+    tags: ['개발', '프로그래밍', '코딩'],
+    createdAt: 'Fri Oct 13 2023 21:51:00 GMT+0900',
     editedAt: 'Fri Oct 13 2023 21:51:00 GMT+0900',
+    likeCnt: 0,
   },
 ];
 
