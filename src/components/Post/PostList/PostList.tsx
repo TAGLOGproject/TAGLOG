b189d79f0db71641bb21899afd65463cdc8b7c77
ssori <115159126+ssori0421@users.noreply.github.com>
2023-10-14 21:21:18 +0900

    feat: 서버로부터 받은 데이터로 UI 수정

--- src/components/Post/PostList/PostList.tsx
+++ src/components/Post/PostList/PostList.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { IPostListData } from '@/types/api/post';
 import styles from './postList.module.scss';
 import Post from '../Post/Post';
 
-function PostList() {
+function PostList({ postList }: { postList: IPostListData[] }) {
   return (
     <div className={styles.container}>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {postList.map((item) => (
+        <Post key={item.post_id} postData={item} />
+      ))}
     </div>
   );
 }
