76f25a030dbfcd5b71fa43f464d3909bf521283c
ssori <115159126+ssori0421@users.noreply.github.com>
2023-10-14 21:22:47 +0900

    feat: 서버로부터 받은 데이터로 UI 교체

--- src/components/Post/Post/Post.tsx
+++ src/components/Post/Post/Post.tsx
@@ -1,30 +1,31 @@
 import Link from 'next/link';
 import Typography from '@/components/Typography';
 import { IPostListData } from '@/types/api/post';
+import { formatTime } from '@/utils/dayjs';
 import styles from './post.module.scss';
 
-function Post(props: IPostListData) {
-  const { post_id: postId } = props;
+function Post({ postData }: { postData: IPostListData }) {
+  const { post_id: postId, title, subtitle, tags, created_at: createdAt } = postData;
 
   return (
     <div className={styles.container}>
       <Link href={`/post-details/${postId}`} />
       <Typography variant="body3" className={styles.postDate}>
-        Oct 11. 2023
+        {formatTime(createdAt)}
       </Typography>
       <Typography variant="title1" className={styles.postTitle}>
-        개발이란
+        {title}
       </Typography>
       <Typography variant="body1" className={styles.postContent}>
-        개발이란 무엇일까
+        {subtitle}
       </Typography>
       <div className={styles.tagContainer}>
-        <div className={styles.tag}>
-          <Typography variant="body4">React</Typography>
-        </div>
-        <div className={styles.tag}>
-          <Typography variant="body4">TypeScript</Typography>
-        </div>
+        {tags &&
+          tags.map((item) => (
+            <div key={item} className={styles.tag}>
+              <Typography variant="body4">{item}</Typography>
+            </div>
+          ))}
       </div>
     </div>
   );
