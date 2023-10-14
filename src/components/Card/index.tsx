7f7a8cff527ae74b20f6271d0e1cd8e55aad6fc7
ssori <115159126+ssori0421@users.noreply.github.com>
2023-10-14 17:52:40 +0900

    feat: useFetchCards 커스텀 훅 적용 및 UI 수정

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 import React from 'react';
+import useFetchCards from '@/hooks/useFetchCards';
 import CardFilter from './CardFilter/CardFilter';
 import CardList from './CardList/CardList';
+import LoadingUI from '../LoadingUI';
 
 function Card() {
+  const { cardsData, isLoading } = useFetchCards();
+  const cards = cardsData;
+
   return (
     <>
-      <CardFilter />
-      <CardList />
+      {isLoading ? null : <CardFilter />}
+      {isLoading ? <LoadingUI type="center" /> : <CardList cards={cards} />}
     </>
   );
 }
