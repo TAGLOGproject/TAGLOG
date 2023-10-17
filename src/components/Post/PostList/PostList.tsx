b926a8c6a791c148e88321a733f2622f329c37b4
ssori <115159126+ssori0421@users.noreply.github.com>
2023-10-17 10:06:21 +0900

    feat: Pagination 컴포넌트 적용

--- src/components/Post/PostList/PostList.tsx
+++ src/components/Post/PostList/PostList.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFilteredPostsStore from '@/store/zustand/useFilteredPostsStore';
+import Pagination from '@/components/Pagination';
+import useFetchPostList from '@/hooks/useFetchPostList';
 import styles from './postList.module.scss';
 import Post from '../Post/Post';
 
 function PostList() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [postsPerPage] = useState<number>(6);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
   const { filteredPostList } = useFilteredPostsStore();
 
+  const currentPagePosts = filteredPostList.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
-    <div className={styles.container}>
-      {filteredPostList.map((item) => (
-        <Post key={item.post_id} postData={item} />
-      ))}
-    </div>
+    <>
+      <div className={styles.container}>
+        {currentPagePosts.map((item) => (
+          <Post key={item.post_id} postData={item} />
+        ))}
+      </div>
+
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalPosts={filteredPostList.length}
+        postsPerPage={postsPerPage}
+      />
+    </>
   );
 }
 
