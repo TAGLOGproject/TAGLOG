a7cc5109e332f334459b19df6109823a8a161349
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-10-26 20:05:01 +0900

    feat: Post로 데이터를 관리하기 위해서 모델 추가

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
-import { IPost } from '@/types/api/post';
 
 const PostSchema = new mongoose.Schema({
   post_id: {
     type: String,
     required: true,
-    trim: true,
   },
   title: {
     type: String,
@@ -35,7 +33,8 @@ const PostSchema = new mongoose.Schema({
   },
   is_temp: {
     type: Boolean,
-    required: false,
+    default: false,
+    required: true,
   },
   comments: {
     type: Object,
@@ -51,6 +50,7 @@ const PostSchema = new mongoose.Schema({
   },
   tags: {
     type: Array,
+    default: [],
   },
   like_count: {
     type: Number,
@@ -63,36 +63,36 @@ const PostSchema = new mongoose.Schema({
 
 export default mongoose.models.post || mongoose.model('post', PostSchema);
 
-export const DATA: IPost = {
-  _id: 'a6ad4723-2e39-46b8-86b3-5eca43abcf4d',
-  title: 'title',
-  body: '> 예시 MOCKING DATA 입니다.\n> \n\n>',
-  thumbnail: 'image url',
-  created_at: '2023-06-21T05:52:25.903Z',
-  edited_at: '2023-07-01T13:45:03.263Z',
-  is_temp: false,
-  comments: [
-    {
-      commentId: '20230101_1',
-      userid: 1,
-      nickname: '닉네임',
-      contents: '댓글 내용',
-      comments: [],
-    },
-  ],
-  comments_count: 0,
-  tags: ['js', 'ts', 'react'],
-  like_count: 0,
-  tag_recommend_post: [
-    {
-      id: 'a6ad4723-2e39-46b8-86b3-5eca43abcf4d',
-      title: '1번글',
-      nickname: '닉네임',
-      created_at: '2023-06-21T05:52:25.903Z',
-      like_count: 0,
-      comments_count: 0,
-    },
-  ],
-  post_id: '',
-  subtitle: '',
-};
+// export const DATA: IPost = {
+//   _id: 'a6ad4723-2e39-46b8-86b3-5eca43abcf4d',
+//   title: 'title',
+//   body: '> 예시 MOCKING DATA 입니다.\n> \n\n>',
+//   thumbnail: 'image url',
+//   created_at: '2023-06-21T05:52:25.903Z',
+//   edited_at: '2023-07-01T13:45:03.263Z',
+//   is_temp: false,
+//   comments: [
+//     {
+//       commentId: '20230101_1',
+//       userid: 1,
+//       nickname: '닉네임',
+//       contents: '댓글 내용',
+//       comments: [],
+//     },
+//   ],
+//   comments_count: 0,
+//   tags: ['js', 'ts', 'react'],
+//   like_count: 0,
+//   tag_recommend_post: [
+//     {
+//       id: 'a6ad4723-2e39-46b8-86b3-5eca43abcf4d',
+//       title: '1번글',
+//       nickname: '닉네임',
+//       created_at: '2023-06-21T05:52:25.903Z',
+//       like_count: 0,
+//       comments_count: 0,
+//     },
+//   ],
+//   post_id: '',
+//   subtitle: '',
+// };
