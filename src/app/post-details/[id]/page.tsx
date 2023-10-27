695611f1e33179e4181080397a7a35875a6bb0b9
ssori <115159126+ssori0421@users.noreply.github.com>
2023-10-27 11:52:20 +0900

    feat: useFetchPost 커스텀 훅 적용

--- src/app/post-details/[id]/page.tsx
+++ src/app/post-details/[id]/page.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+import useFetchPost from '@/hooks/useFetchPost';
 import PostDetails from './PostDetails';
+import LoadingUI from '../../../components/LoadingUI';
 
 function PostDetailsPage() {
-  return <PostDetails />;
+  const { isLoading } = useFetchPost();
+
+  return isLoading ? <LoadingUI type="component" /> : <PostDetails />;
 }
 
 export default PostDetailsPage;
