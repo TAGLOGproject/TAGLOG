0b1b935c01cbfbe6fe885c31cc2b6a976c466642
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-11-01 23:56:42 +0900

    error: 무한 요청 수정

--- src/hooks/useFetchPost.tsx
+++ src/hooks/useFetchPost.tsx
@@ -1,21 +1,19 @@
 'use client';
 
-import React, { useState, useEffect, useCallback } from 'react';
-import { useParams } from 'next/navigation';
+import { useState, useEffect, useCallback } from 'react';
 import { toast } from 'react-toastify';
 import { AxiosError } from 'axios';
 import { getPostApi } from '@/service/post';
 import { IPost } from '@/types/api/post';
 
-const useFetchPost = () => {
+const useFetchPost = (postId: number) => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<IPost | IPost[]>();
-  const params = useParams();
 
   const getPost = useCallback(async () => {
     try {
-      if (!params) return;
-      const postId = Number(params.id);
+      if (!postId) return;
+
       const result = await getPostApi(postId);
       setData(result);
       setIsLoading(false);
@@ -23,7 +21,7 @@ const useFetchPost = () => {
       setIsLoading(false);
       toast.error((error as AxiosError).message);
     }
-  }, [params]);
+  }, [postId]);
 
   useEffect(() => {
     getPost();
