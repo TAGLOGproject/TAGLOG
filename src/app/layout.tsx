9c6f5e7aca20f69ba834573acf6cfc3945c97c96
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-11-01 20:54:05 +0900

    chore: 메타태그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,11 @@ import ToastProvider from '@/components/ToastProvider';
 
 export const metadata = {
   title: 'TAGLOG',
-  description: '태그를 활용한 블로그',
+  openGraph: {
+    title: 'TAGLOG',
+    description: '태그를 활용한 프론트 개발자들의 기술 블로그',
+  },
+  description: '태그를 활용한 프론트 개발자들의 기술 블로그',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
