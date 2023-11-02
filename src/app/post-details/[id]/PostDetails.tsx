c66a96e666964ae69203aec70a9abf5b10369958
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-11-02 18:21:43 +0900

    design: Ui, text 수정

--- src/app/post-details/[id]/PostDetails.tsx
+++ src/app/post-details/[id]/PostDetails.tsx
@@ -8,7 +8,7 @@ import styles from './postDetails.module.scss';
 
 function PostDetails({ data }: { data: any }) {
   const postData = { ...data } as IPost;
-  const { title, tags, body } = postData;
+  const { title, tags, body, thumbnail } = postData;
   return (
     <div className={styles.container}>
       <Typography variant="h1" className={styles.title}>
@@ -19,12 +19,13 @@ function PostDetails({ data }: { data: any }) {
           <Tag key={tag} tag={tag} />
         ))}
       </div>
+      {thumbnail && (
+        <div className={styles.imageWrapper}>
+          <Image src={thumbnail} alt="thumbnail" width={600} height={400} />
+        </div>
+      )}
 
-      <div className={styles.imageWrapper}>
-        <Image src={postData.thumbnail} alt="thumbnail" width={600} height={400} />
-      </div>
-
-      <MarkdownBlogViewer postData={postData.body} />
+      <MarkdownBlogViewer postData={body} />
     </div>
   );
 }
