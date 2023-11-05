b5cf53be969c5d37ccfee847a44690e635ade1d6
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-11-05 21:52:26 +0900

    feat: axios intercepter 구현 토큰 관련 처리

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { getAccessToken } from '@/utils/frontend/localstorage';
+import { toast } from 'react-toastify';
 
 const defaultConfig = {
   baseURL: `/api`,
@@ -11,12 +12,25 @@ const defaultConfig = {
 export const instance = axios.create(defaultConfig);
 
 export const authInstance = axios.create(defaultConfig);
+
 authInstance.interceptors.request.use((config) => {
   const accessToken = getAccessToken();
+
+  if (!accessToken) {
+    if (typeof window !== 'undefined') {
+      toast.error('토큰이 없습니다.');
+      setTimeout(() => {
+        window.location.href = '/'; // 로그인 페이지로 리디렉트합니다.
+      }, 1000);
+    }
+
+    return Promise.reject(new Error('토큰이 없습니다.'));
+  }
   const newConfig = { ...config };
   if (accessToken) {
-    const token = JSON.parse(accessToken);
+    const token = typeof accessToken === 'string' ? accessToken : JSON.stringify(accessToken);
     newConfig.headers.Authorization = `Bearer ${token}`;
   }
+
   return newConfig;
 });
