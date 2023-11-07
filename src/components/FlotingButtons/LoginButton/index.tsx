d3923077adeeb50f66ab3255603cfed1d1167fa0
ssori <115159126+ssori0421@users.noreply.github.com>
2023-11-07 16:30:54 +0900

    feat: 로그아웃 드롭다운 기능 구현 및 스타일링

--- src/components/FlotingButtons/LoginButton/index.tsx
+++ src/components/FlotingButtons/LoginButton/index.tsx
@@ -1,19 +1,62 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useModalStore from '@/store/zustand/useModalStore';
+import useStore from '@/store/zustand/useStore';
+import useAuthStore from '@/store/zustand/useAuthStore';
+import Typography from '@/components/Typography';
 import styles from './loginButtons.module.scss';
 
 export default function LoginButton() {
+  const [isDropdownVisible, setDropdownVisible] = useState<boolean>(false);
+
   const setModal = useModalStore((state) => state.setModal);
+  const userInfo = useStore(useAuthStore, (state) => state.userInfo);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleModal = () => {
     setModal();
   };
 
+  const handleSignOutDropDown = () => {
+    setDropdownVisible(true);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setDropdownVisible(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <button className={styles.loginButton} type="button" onClick={handleModal}>
-      로그인
-    </button>
+    <div className={styles.container}>
+      {userInfo ? (
+        <button type="button" className={styles.userInfo} onClick={handleSignOutDropDown}>
+          <Typography variant="body2">{userInfo.email}</Typography>
+        </button>
+      ) : (
+        <button className={styles.loginButton} type="button" onClick={handleModal}>
+          <Typography variant="body2"> 로그인</Typography>
+        </button>
+      )}
+
+      {isDropdownVisible && (
+        <div className={styles.dropdown} ref={dropdownRef}>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              <Typography variant="body2">로그아웃</Typography>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
